7b6eb0e08b6a61a3b3bb2e036e2e7f541fbda7c4
Aaron Haag <101981364+AMHaag@users.noreply.github.com>
2022-04-13 23:28:16 -0400

    more git practice

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,10 @@
-window.alert("Test Success")
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+fight();
+
+var playerName = window.prompt("What is your robot's name?");
+
+console.log("Your robot's name is " + playerName + "!");
